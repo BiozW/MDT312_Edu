bd0b5e905f3a3f2530d9c96825769e40a32b1314
BiozW <jirayusuk@gmail.com>
2023-09-12 11:20:59 +0700

    EDIT: Register again

--- assignment6/register.js
+++ assignment6/register.js
@@ -1,15 +1,25 @@
 window.onload = pageLoad;
 
-function A() {
-    document.getElementById()
-}
 function pageLoad(){
 	var form = document.getElementById("myForm");
     form.onsubmit = validateForm;
+    form.onreset = reform;
 }
 
 function validateForm() {
-    if (document.getElementById("passwordA") == document.getElementById("passwordB"))
-    alert("Accept Error")
+    var a = document.getElementById('passwordA');
+    var b = document.getElementById('passwordB');
+
+    if (a.value != b.value)
+    {
+        alert("กรอกไม่ถูกเว้ย")
+        document.getElementById('errormsg')
+        return false;
+    }
+
     //ถ้าตรวจสอบแล้วว่ามีการ register ไม่ถูกต้องให้ return false ด้วย
+}
+
+function reform(){
+    document.getElementById("myForm") = null;
 }
