49918241172f179a2c214ed35eecfbbbdc12f077
BiozW <jirayusuk@gmail.com>
2023-10-17 02:12:20 +0700

    EDIT: can read json

--- assignment9/server.js
+++ assignment9/server.js
@@ -7,13 +7,23 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    
-    
+    readMsg((data) => {
+        res.write(data);
+        res.end();
+    });
   });
 
-let readMsg = () => {
-    
+let readMsg = (callback) => {
+    fs.readFile('cloth1.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            callback('Error reading the file');
+        } else {
+            callback(data);
+        }
+    });
 }
+
 
 // จำนวนเสื้อผ้าตามที่กำหนด
 let editJson = (data) => { 
@@ -28,11 +38,26 @@ let editJson = (data) => {
         item8: 29,
         item9: 10
     }
+    fs.writeFile('cloth1.json', JSON.stringify(stock), 'utf8', (err) => {
+        if (err) {
+            console.error(err);
+            callback('Error writing the file');
+        } else {
+            writeMsg(stock, callback);
+        }
+    });
 
 }
 
 let writeMsg = () =>{
-    
+    fs.writeFile('new_cloth.json', JSON.stringify(data), 'utf8', (err) => {
+        if (err) {
+            console.error(err);
+            callback('Error writing the new file');
+        } else {
+            callback(null);
+        }
+    });
 }
 
 server.listen(port, hostname, () => {
